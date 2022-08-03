6013013a2d45df5d7fffa12587d4f365b243531f
djserega <galyuk.sergey@gmail.com>
2022-08-03 21:35:14 +0300

    Initial commit

---
+++ src/DragDropImage/DragDropImage/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DragDropImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] pathImage = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string pathFile in pathImage)
+                {
+                    UserImage newWindow = new();
+                    newWindow.ImageFullName.Source = new BitmapImage(new Uri(pathFile));
+                    newWindow.Show();
+                    newWindow.Activate();
+                }
+            }
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
---
+++ src/DragDropImage/DragDropImage/UserImage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DragDropImage
+{
+    /// <summary>
+    /// Interaction logic for UserImage.xaml
+    /// </summary>
+    public partial class UserImage : Window
+    {
+        public UserImage()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageFullName_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
